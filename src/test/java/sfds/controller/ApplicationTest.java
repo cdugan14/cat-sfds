662effadce6535746372f3b792dd51732d30e5b7
Cat Dugan <cdugan@thoughtworks.com>
2019-09-09 21:28:28 -0700

    Refactor package structure to better separate controllers into their own package

--- src/test/java/sfds/controller/ApplicationTest.java
+++ src/test/java/sfds/controller/ApplicationTest.java
@@ -14,11 +14,7 @@
  * limitations under the License.
  */
 
-package hello;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+package sfds.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,6 +22,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = GreetingController.class)
